0470a6a023472ccaa8ae058e820f80c5d0976032
barbaalghul <anderson.stan5@gmail.com>
2018-05-09 17:20:07 -0300

    changed send_email method, updated requirements and updated gitignore

--- send_email.py
+++ send_email.py
@@ -1,10 +1,11 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.mime.image import MIMEImage
+from email.mime.base import MIMEBase
+from email import encoders
 
 
-def send():
+def send(files):
     from_addr = "E-MAIL HERE"
     to_addr = "E-MAIL HERE"
 
@@ -12,21 +13,21 @@ def send():
 
     msg['From'] = from_addr
     msg['To'] = to_addr
-    msg['Subject'] = "TREINAMENTO DA REDE CONCLUÍDO"
+    msg['Subject'] = "Treinamento da Rede Neural concluído!"
 
     # abre o arquivo results e coloca o conteúdo como corpo do e-mail
     with open('results.txt', 'r') as fp:
         body = fp.read()
-    fp.close()
 
     msg.attach(MIMEText(body, 'plain'))
 
-    # anexa o gráfico training loss
-    with open('training_loss.png', 'rb') as fp:
-        img = MIMEImage(fp.read())
-    fp.close()
-    img.add_header('Content-Disposition', 'attachment', filename='training_loss.png')
-    msg.attach(img)
+    for i in range(0, len(files)):
+        with open(files[i], 'rb') as fp:
+            file_attc = MIMEBase('application', "octet-stream")
+            file_attc.set_payload(fp.read())
+        encoders.encode_base64(file_attc)
+        file_attc.add_header('Content-Disposition', 'attachment', filename=files[i])
+        msg.attach(file_attc)
 
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
